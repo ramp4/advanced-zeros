96199dfb97a8290ff5fe1c8ee72bebb1a1d8888a
ramp4 <rampatv13@gmail.com>
2019-03-24 19:25:33 +0300

    feat: add fuction that gets Zeros count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let divider = [];
+  let dividerPair = {};
+  let factorsBefore = 1;
+  let result = 1;
+  let limit = Math.ceil(Math.sqrt(base));
+  let dividerCount = {};
+  
+  for (i = 2; i < limit  ; i++) {
+    if (base % i == 0 ) {
+      result++;
+      divider.push(i); 
+      quotient = base / i;
+      divider.unshift(quotient); 
+      dividerCount[i] = 0;
+      dividerCount[quotient] = 0;
+      dividerPair[i]=quotient;
+      dividerPair[quotient]=i;
+    };
+  }
+
+  let dividerLenght = divider.length;
+  divider.unshift(base);
+  console.log(divider);
+  
+  for (let i = base + 1; i <=  number; i++) {
+    for (let j = 0; j <= dividerLenght; j++ ) {
+      let factor = i; 
+      while (factor % divider[j] == 0) {
+        factor =factor / divider[j];
+        dividerCount[divider[j]]++;
+        if ((dividerCount[dividerPair[j]] > 0 ) && (dividerCount[j] > 0)) {
+          dividerCount[dividerPair[j]]--;
+          dividerCount[j]--;
+          result++;
+        }
+      };
+    };
+  };
+  console.log(dividerCount);
+  console.log(result);
+   return result;  
 }
